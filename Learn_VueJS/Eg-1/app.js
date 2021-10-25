bcc0f546faf464c3bff8f89aabcef6533453d478
Anandhakrishnan1 <48061806+Anandhakrishnan1@users.noreply.github.com>
2021-10-25 19:51:06 +0530

    added vuejs

---
+++ Learn_VueJS/Eg-1/app.js
@@ -0,0 +1,14 @@
+Vue.createApp({
+    data() {
+        return {
+            tasks: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addTask() {
+            this.tasks.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
