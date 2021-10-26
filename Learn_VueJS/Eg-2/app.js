ce3598227aae52d184cf01c836dfb54ff3dcf1c8
Anandhakrishnan1 <48061806+Anandhakrishnan1@users.noreply.github.com>
2021-10-26 16:01:33 +0530

    first method

--- Learn_VueJS/Eg-2/app.js
+++ Learn_VueJS/Eg-2/app.js
@@ -4,6 +4,18 @@ const app = Vue.createApp({
             courseGoal:  'Finish this course and be an expert !',
             vueLink: 'https://vuejs.org/'
         };
-    }
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if(randomNumber < 0.5) {
+                return 'learn Vue!';
+            }
+            else {
+                return 'master Vue!';
+            }
+        }
+    },
+
 });
 app.mount('#user-goal');
